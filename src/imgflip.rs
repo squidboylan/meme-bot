6bf37e64955dae325e25717f4926adaea3bcbb0c
Caleb Boylan <calebboylan@gmail.com>
2020-12-27 13:27:41 -0800

    Break template map out and add a watcher to reload it
    THIS IS HACKS AND SHOULD BE REWORKED AT SOME POINT PROBABLY

--- src/imgflip.rs
+++ src/imgflip.rs
@@ -1,6 +1,7 @@
 use reqwest::Result;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use tokio::fs;
 
 #[derive(Clone, Debug, Serialize)]
 struct CaptionPost<'a> {
@@ -29,32 +30,42 @@ pub enum CaptionResponseData {
 pub struct ImgflipClient {
     pub username: String,
     pub password: String,
-    meme_map: HashMap<&'static str, &'static str>,
+    meme_map: HashMap<String, String>,
+}
+
+async fn load_template_map(template_map_file: &str) -> Option<HashMap<String, String>> {
+    let contents_result = fs::read_to_string(template_map_file).await;
+    if let Ok(contents) = contents_result {
+        let tmp_meme_map = serde_json::from_str::<HashMap<String, String>>(&contents);
+        if let Ok(meme_map) = tmp_meme_map {
+            Some(meme_map)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
 }
 
 impl ImgflipClient {
-    pub fn new(username: String, password: String) -> Self {
-        let meme_map: HashMap<&'static str, &'static str> = [
-            ("drake", "181913649"),
-            ("always has been", "252600902"),
-            ("distracted boyfriend", "112126428"),
-            ("spongebob", "102156234"),
-            ("sharp turn", "124822590"),
-            ("big brain", "93895088"),
-            ("aliens", "101470"),
-            ("two buttons", "87743020"),
-            ("mask reveal", "145139900"),
-            ("awkward muppet", "195162885"),
-            ("evil kermit", "84341851"),
-            ("gru's plan", "131940431"),
-        ]
-        .iter()
-        .cloned()
-        .collect();
+    pub async fn new(username: String, password: String, template_map_file: &str) -> Self {
+        let meme_map = load_template_map(&template_map_file)
+            .await
+            .expect("Failed to load template_map");
         ImgflipClient {
             username,
             password,
             meme_map,
+        }
+    }
+
+    pub async fn update_template_map(&mut self, template_map_file: &str) {
+        let meme_map_o = load_template_map(&template_map_file).await;
+        if let Some(meme_map) = meme_map_o {
+            self.meme_map = meme_map;
+            println!("Loaded new template_map");
+        } else {
+            println!("Failed to load new template_map, still using the old one");
         }
     }
 
@@ -67,7 +78,7 @@ impl ImgflipClient {
         let lowercase_template_name = template_name.to_lowercase();
         let id = self.meme_map.get::<str>(&lowercase_template_name).unwrap();
         let mut params: Vec<(String, &str)> = vec![
-            ("template_id".to_string(), *id),
+            ("template_id".to_string(), &id),
             ("username".to_string(), &self.username),
             ("password".to_string(), &self.password),
         ];
@@ -84,6 +95,6 @@ impl ImgflipClient {
     }
 
     pub fn list_memes<'a>(&'a self) -> Vec<&'a str> {
-        self.meme_map.keys().copied().collect()
+        self.meme_map.keys().map(|x| x.as_ref()).collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use inotify::{Inotify, WatchMask};
 use std::env;
 use std::sync::Arc;
+use tokio::stream::StreamExt;
 use tokio::sync::RwLock;
 
 use serenity::prelude::*;
@@ -12,17 +14,49 @@ pub struct ImgflipClientContainer;
 impl TypeMapKey for ImgflipClientContainer {
     type Value = Arc<RwLock<imgflip::ImgflipClient>>;
 }
+
+async fn update_template_map(
+    template_map_file: String,
+    imgflip_client: Arc<RwLock<imgflip::ImgflipClient>>,
+) -> ! {
+    let mut inotify = Inotify::init().expect("Failed to initialize inotify");
+
+    inotify
+        .add_watch(
+            template_map_file.clone(),
+            WatchMask::CREATE | WatchMask::MODIFY,
+        )
+        .unwrap();
+
+    let mut buffer = [0; 32];
+    let mut stream = inotify.event_stream(&mut buffer).unwrap();
+    loop {
+        while let Some(event_or_error) = stream.next().await {
+            let mut lock = imgflip_client.write().await;
+            lock.update_template_map(&template_map_file).await;
+            println!("event: {:?}", event_or_error.unwrap());
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let username = env::var("IMGFLIP_USERNAME").expect("Expected a token in the environment");
-    let password = env::var("IMGFLIP_PASSWORD").expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Please set DISCORD_TOKEN");
+    let username = env::var("IMGFLIP_USERNAME").expect("Please set IMGFLIP_USERNAME");
+    let password = env::var("IMGFLIP_PASSWORD").expect("Please set IMGFLIP_PASSWORD");
+    let template_map = env::var("TEMPLATE_MAP")
+        .or::<std::env::VarError>(Ok("template_map.json".to_string()))
+        .unwrap();
 
-    let imgflip_client = Arc::new(RwLock::new(imgflip::ImgflipClient::new(
-        username.to_string(),
-        password.to_string(),
-    )));
+    let imgflip_client = Arc::new(RwLock::new(
+        imgflip::ImgflipClient::new(username.to_string(), password.to_string(), &template_map)
+            .await,
+    ));
+    tokio::spawn(update_template_map(
+        template_map.clone(),
+        Arc::clone(&imgflip_client),
+    ));
 
     // Create a new instance of the Client, logging in as a bot. This will
     // automatically prepend your bot token with "Bot ", which is a requirement
